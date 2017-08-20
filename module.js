fdbe4edd769e4f6ff9d2925eb41d5200a2e8828d
teddy <te@s.com>
2017-08-20 17:35:19 +0800

    1

--- module.js
+++ module.js
@@ -492,7 +492,26 @@ define('directives/testDirective',['require', './module'], function(require, mod
 	});
 
 });
-define('directives/main',['./testDirective'],
+define('directives/loadscript',['require', './module'], function(require, module) {
+	'use strict';
+
+	module.directive("script", function() {
+		return {
+			restrict: 'E',
+			scope: false,
+			link: function(scope, elem, attr) {
+				if(attr.type === 'text/javascript-lazy') {
+					var code = elem.text();
+					var f = new Function(code);
+					f();
+				}
+			}
+		};
+
+	});
+
+});
+define('directives/main',['./testDirective', './loadscript'],
 	function() {
 
 		'use strict';
